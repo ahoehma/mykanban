c1d3653197eeaa14c8a6564c16dbecfb8add2ec7
Andreas Höhmann <andreas.hoehmann@gmx.de>
2015-10-16 00:42:31 +0200

    [init] generated with http://start.spring.io/

---
+++ mykanban/src/main/java/de/ahoehma/MykanbanApplication.java
@@ -0,0 +1,12 @@
+package de.ahoehma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykanbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MykanbanApplication.class, args);
+    }
+}
